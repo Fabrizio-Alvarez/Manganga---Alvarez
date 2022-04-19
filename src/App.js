19fcc65416bcb8d75fffbeded9b333a69345fc22
Fabrizio-Alvarez <fabrizioalvrz@gmail.com>
2022-04-18 23:33:17 -0300

    commit navbar actividad 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <p>Hola mundo</p>
+          <Navbar />
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <div>
+        <ul id="nav">
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Merchandising</a></li>
+          <li><a href="#">Lineup</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </div> 
+  )
+}
+
+export default Navbar
